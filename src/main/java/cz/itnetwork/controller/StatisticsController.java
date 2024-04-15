87144c4522fc00fe4b2d44b360ad43e9ef9eba2e
Kat <klicova.katerina@seznam.cz>
2024-04-15 22:44:38 +0200

    javadoc for StatisticsController written, class checked

--- src/main/java/cz/itnetwork/controller/StatisticsController.java
+++ src/main/java/cz/itnetwork/controller/StatisticsController.java
@@ -10,18 +10,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * This rest controller catches http requests to url addresses starting with `api`
+ *
+ * @author Kat
+ */
 @RestController
 @RequestMapping("/api")
 public class StatisticsController {
 
+    /**
+     * Represents an autowired bean of {@link StatisticsService} needed
+     */
     @Autowired
     private StatisticsService statisticsService;
 
+    /**
+     * Shows statistics for invoices
+     * Catches http GET method to url ending with `/invoices/statistics`
+     * @return data filtered at database level as {@link InvoicesStatisticsDTO}
+     */
     @GetMapping("/invoices/statistics")
     public InvoicesStatisticsDTO getInvoicesStatistics() {
         return statisticsService.getInvoicesStatistics();
     }
 
+    /**
+     * Shows statistics of individual people/companies
+     * Catches http GET method to url ending with `/persons/statistics`
+     * @return data filtered at the database level as list of {@link PersonsStatisticsDTO}
+     */
     @GetMapping("/persons/statistics")
     public List<PersonsStatisticsDTO> getPersonsStatistics() {
         return statisticsService.getPersonsStatistics();
