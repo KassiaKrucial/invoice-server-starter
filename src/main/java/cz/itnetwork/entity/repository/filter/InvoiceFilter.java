698b9d7d2b23b33f1f2b4702bfa88af206f3ef0d
Kat <klicova.katerina@seznam.cz>
2024-04-16 10:00:47 +0200

    javadoc for InvoiceFilter written, class checked

--- src/main/java/cz/itnetwork/entity/repository/filter/InvoiceFilter.java
+++ src/main/java/cz/itnetwork/entity/repository/filter/InvoiceFilter.java
@@ -2,14 +2,50 @@ package cz.itnetwork.entity.repository.filter;
 
 import lombok.Data;
 
+/**
+ * This class defines filtering parameters of invoices
+ * Getters, setters, constructor generated with lombok
+ *
+ * @author Kat
+ */
 @Data
 public class InvoiceFilter {
 
+    /**
+     * <p>Represents the id of a buyer</p>
+     * <p>Selected by user</p>
+     */
     private Long buyerID;
+
+    /**
+     * <p>Represents the id of a seller</p>
+     * <p>Selected by user</p>
+     */
     private Long sellerID;
+
+    /**
+     * <p>Represents name of a product</p>
+     * <p>Written by user</p>
+     */
     private String product;
+
+    /**
+     * <p>Represents the minimal price of a product</p>
+     * <p>Written by user</p>
+     */
     private Long minPrice;
+
+    /**
+     * <p>Represents the maximal price of a product</p>
+     * <p>Written by user</p>
+     */
     private Long maxPrice;
+
+    /**
+     * <p>Limits the total of results</p>
+     * <p>Written by user</p>
+     * <p>Default value is {@code 10}</p>
+     */
     private Integer limit = 10;
 
 }
