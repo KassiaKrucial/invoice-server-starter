ea00ccd602a0097041c873c28faeefc8ea087072
Kat <klicova.katerina@seznam.cz>
2024-04-16 11:20:09 +0200

    javadoc for InvoiceRepository written, class checked

--- src/main/java/cz/itnetwork/entity/repository/InvoiceRepository.java
+++ src/main/java/cz/itnetwork/entity/repository/InvoiceRepository.java
@@ -1,29 +1,45 @@
 package cz.itnetwork.entity.repository;
 
-import cz.itnetwork.dto.InvoiceDTO;
 import cz.itnetwork.dto.InvoicesStatisticsDTO;
 import cz.itnetwork.dto.PersonsStatisticsDTO;
 import cz.itnetwork.entity.InvoiceEntity;
-import cz.itnetwork.entity.PersonEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 
+/**
+ * This interface CRUD operations and written sql queries for invoices
+ *
+ * @author Kat
+ */
 public interface InvoiceRepository extends
         JpaRepository<InvoiceEntity, Long>,
         JpaSpecificationExecutor<InvoiceEntity> {
 
+    /**
+     * <p>Finds invoices by seller</p>
+     * <p>Selected by user</p>
+     * @param sellerId The id of an invoice
+     * @return List of filtered invoices
+     */
     @Query(value = "SELECT invoice.* FROM invoice invoice INNER JOIN person person ON invoice.seller_id = person.id WHERE invoice.seller_id = :sellerId", nativeQuery = true)
     List<InvoiceEntity> findBySeller(@Param("sellerId") long sellerId);
 
+    /**
+     * <p>Finds invoices by buyer</p>
+     * <p>Selected by user</p>
+     * @param buyerId
+     * @return
+     */
     @Query(value = "SELECT invoice.* FROM invoice invoice INNER JOIN person person ON invoice.buyer_id = person.id WHERE invoice.buyer_id = :buyerId", nativeQuery = true)
     List<InvoiceEntity> findByBuyer(@Param("buyerId") long buyerId);
 
+    /**
+     * Sums revenues for current year, every year and counts total number of invoices in the database
+     * @return According sums of revenues and count of invoices as {@link InvoicesStatisticsDTO}
+     */
     @Query(value = """
         SELECT NEW cz.itnetwork.dto.InvoicesStatisticsDTO(
             SUM(currentYearInvoices.price), 
@@ -36,6 +52,10 @@ public interface InvoiceRepository extends
         """)
     InvoicesStatisticsDTO getInvoicesStatistics();
 
+    /**
+     * Sums revenues for each person/company
+     * @return List of according sums of revenues, person/company id and name as {@link PersonsStatisticsDTO}
+     */
     @Query(value = """
         SELECT NEW cz.itnetwork.dto.PersonsStatisticsDTO(
             person.id, person.name, SUM(invoice.price)) 
@@ -44,6 +64,4 @@ public interface InvoiceRepository extends
         GROUP BY person.id
         """)
     List<PersonsStatisticsDTO> getPersonsStatistics();
-
-
 }
