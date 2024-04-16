6f3c0e2657075c3d252c600169584da3506da12a
Kat <klicova.katerina@seznam.cz>
2024-04-16 09:32:19 +0200

    javadoc for EntityNotFoundExceptionAdvice written, class not mine

--- src/main/java/cz/itnetwork/controller/advice/EntityNotFoundExceptionAdvice.java
+++ src/main/java/cz/itnetwork/controller/advice/EntityNotFoundExceptionAdvice.java
@@ -7,9 +7,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.webjars.NotFoundException;
 
+/**
+ * This class defines what happens when NotFoundException and EntityNotFoundException are thrown
+ *
+ * @author ITnetwork
+ * @author The author of Javadoc for this class is Kat
+ */
 @ControllerAdvice
 public class EntityNotFoundExceptionAdvice {
 
+    /**
+     * Handles NotFoundException and EntityNotFoundException
+     * returns http response status 404 - not found
+     */
     @ExceptionHandler({NotFoundException.class, EntityNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public void handleEntityNotFoundException() {
