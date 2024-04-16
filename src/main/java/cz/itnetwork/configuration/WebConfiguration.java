307763617cb319d575d95d9a3bef3efb80fbece7
Kat <klicova.katerina@seznam.cz>
2024-04-16 09:27:14 +0200

    javadoc for WebConfiguration written, class not mine

--- src/main/java/cz/itnetwork/configuration/WebConfiguration.java
+++ src/main/java/cz/itnetwork/configuration/WebConfiguration.java
@@ -5,10 +5,24 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * This configuration class adds CORS mapping
+ * Allows access to resources from external third parties
+ *
+ * @author ITnetwork
+ * @author The author of Javadoc for this class is Kat
+ */
 @Configuration
 @EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer {
 
+    /**
+     * Defines global CORS configurations:
+     *      adds mapping to `/**`
+     *      allows http methods: HEAD, GET, POST, PUT, DELETE, OPTIONS
+     *      all credentials are true
+     * @param registry
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         WebMvcConfigurer.super.addCorsMappings(registry);
